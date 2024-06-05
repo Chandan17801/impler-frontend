03a22e2c98349f58accb9695e5977424d75a1698
Chandan17801 <chandansingh17801@gmail.com>
2024-06-05 14:11:31 +0530

    "Updated ProductCard component: adjusted image margin, changed font sizes and styles for product name, description, and price, and modified button styles."

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -20,11 +20,11 @@ const ProductCard = ({ product }) => {
       <img
         src={product.image}
         alt={product.name}
-        className="w-full h-48 object-cover rounded-lg mb-4"
+        className="w-full h-48 object-cover rounded-lg mb-2"
       />
-      <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
-      <p className="text-gray-700 mb-2">{product.description}</p>
-      <p className="text-lg font-bold mb-4">{formatToINR(product.price)}</p>
+      <h2 className="text-lg font-semibold mb-2">{product.name}</h2>
+      <p className="text-gray-700 text-base text-justify mb-2">{product.description}</p>
+      <p className="text-lg font-bold mb-2">{formatToINR(product.price)}</p>
       <button
         onClick={onClickAddToCartHandler}
         className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600"
